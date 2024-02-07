3accb068e2d995a2115c33fb284f5268811291a8
Sergio del Amo <sergio.delamo@softamo.com>
2024-02-07 10:08:54 +0100

    use @Status to simplify controller

--- src/main/groovy/mn/ser/security/MyController.groovy
+++ src/main/groovy/mn/ser/security/MyController.groovy
@@ -2,10 +2,12 @@ package mn.ser.security
 
 import groovy.transform.CompileStatic
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Status
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
 
@@ -15,8 +17,8 @@ class MyController {
 
     @Get
     @Secured(SecurityRule.IS_AUTHENTICATED)
-    HttpResponse ping() {
-        return HttpResponse.ok()
+    @Status(HttpStatus.OK)
+    void ping() {
     }
 
     @Post('/pet')
